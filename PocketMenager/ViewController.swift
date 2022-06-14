7ea3ba8fe49fc201fb4a2af864de50fbf9dfbf32
Bartosz <barfira321@icloud.com>
2022-06-14 12:03:21 +0200

    Initial Commit

---
+++ PocketMenager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PocketMenager
+//
+//  Created by Bartek Fira on 14/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
